b89e44fa78bb160209d17e4bdc6bd8f6d6b4684c
KangJiJi <kjhmd9726@gmail.com>
2020-08-07 15:38:59 +0900

    [Fix] API 주소 변경

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,42 +1,44 @@
-import * as types from './ActionTypes'
+import * as types from "./ActionTypes";
 
 export function changeFirstLoadingState() {
-    // Use this function to display animation page.
-    return {
-        type: types.CHANGE_FIRST_LOADING_STATE
-    };
+  // Use this function to display animation page.
+  return {
+    type: types.CHANGE_FIRST_LOADING_STATE,
+  };
 }
 
 export function changeInitScrollEvent() {
-    // Return object.
-    return {
-        type: types.CHANGE_INIT_SCROLL_EVENT
-    };
+  // Return object.
+  return {
+    type: types.CHANGE_INIT_SCROLL_EVENT,
+  };
 }
 
-export const getMovies = (sort, page) => dispatch => {
-    dispatch({
-        type: types.GET_POST_PENDING,
-        page: page // Variable to see if sort has changed
-    });
-    return (
-        // Use fetch API for network request.
-        fetch(`https://yts.am/api/v2/list_movies.json?&sort_by=${sort}&page=${page}`)
-        .then(response => response.json())
-        .then(json => {
-            dispatch({
-                type: types.GET_POST_SUCCESS,
-                newMovies: json.data.movies,
-                sort: sort,
-                page: page
-            })
-        })
-        .catch(err => {
-            // Exceptions handling.
-            dispatch({
-                type: types.GET_POST_FAILURE,
-                error: err
-            })
-        })
-    );
+export const getMovies = (sort, page) => (dispatch) => {
+  dispatch({
+    type: types.GET_POST_PENDING,
+    page: page, // Variable to see if sort has changed
+  });
+  return (
+    // Use fetch API for network request.
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?&sort_by=${sort}&page=${page}`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        dispatch({
+          type: types.GET_POST_SUCCESS,
+          newMovies: json.data.movies,
+          sort: sort,
+          page: page,
+        });
+      })
+      .catch((err) => {
+        // Exceptions handling.
+        dispatch({
+          type: types.GET_POST_FAILURE,
+          error: err,
+        });
+      })
+  );
 };
